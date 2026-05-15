c0025357a7af038734fd54161953c95953514398
samuele de bona <samueledb06@gmail.com>
2026-05-15 11:21:21 +0200

    aggiunra della pagina per il caricamento del forum

---
+++ forum.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "function.php";
+
+session_start();
+$email = $_SESSION['email'] ?? null;
+$tipo = $_SESSION['tipo'] ?? null;
+
+if(!isset($email, $tipo)) {
+    header('Location: login.php');
+    exit;
+}
+
+$forum_id = $_REQUEST['forum_id'] ?? null;
+$forum = getForumByForumId($forum_id);
+
+if ($forum_id === null || $forum === false) {
+    if($tipo === 'scuola') {
+        header('Location: homepage_scuola.php');
+    } else {
+        header('Location: homepage_utente.php');
+    }
+    exit;
+}
+
+
