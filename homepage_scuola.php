ad17922e7917ccae38f491ce7ad84d7209a7d8ad
Cris2194 <cristianroni92@gmail.com>
2026-05-15 12:54:55 +0200

    Modifica Homepage Scuola * pagina per modificare profilo

--- homepage_scuola.php
+++ homepage_scuola.php
@@ -9,19 +9,45 @@ $email = $_SESSION['email'] ?? '';
 $nome = $_SESSION['nome'] ?? '';
 $tipo = $_SESSION['tipo'] ?? '';
 
+$ultimi30Forum = getLast30Forum();
+
 ?>
 <!DOCTYPE html>
-<html lang="it">      
-    <head>
-        <title>Homepage Scuola</title>
-    </head>
+<html lang="it">
+<head>
+    <title>Homepage Scuola</title>
+</head>
+<body>
+    <h1>Benvenuto, <?php echo $nome; ?>!</h1>
+    <p>Questa è la homepage del tua scuola.</p>
 
-    <body>
-        <h1>Benvenuto, <?php echo $nome; ?>!</h1>
-        <p>Questa è la homepage della tua scuola.</p>
+    <a href="logout.php">Logout</a>
+    <br>
+    <a href="modifica_profilo_scuola.php">Modifica Profilo</a>
 
-        <a href="logout.php">Logout</a>
-    </body>
+    <table>
+        <thead>
+            <tr>
+                <th>Titolo</th>
+                <th>Username</th>
+                <th>Data di Creazione</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($ultimi30Forum as $forum) { ?>
+                <tr>
+                    <td>
+                        <a href="forum.php?forum_id=<?= $forum['forum_id'] ?>">
+                            <?php echo htmlspecialchars($forum['titolo']); ?>
+                        </a>
+                    </td>
+                    <td><?php echo htmlspecialchars($forum['username']); ?></td>
+                    <td><?php echo htmlspecialchars($forum['data_pubblicazione']); ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
 </html>
 
 
---
+++ modifica_profilo_scuola.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Modifica Profilo Scuola</title>
+    </head>
+    <body>
+        <h1>Modifica Profilo Scuola</h1>
+        <!-- Form per la modifica del profilo della scuola -->
+    </body>
+</html>
