5ebac16057b6565efe8dfa6e95f61a01259292c0
samuele de bona <samueledb06@gmail.com>
2026-05-15 11:52:55 +0200

    Aggiunta

--- forum.php
+++ forum.php
@@ -25,6 +25,10 @@ if ($forum_id === null || $forum === false) {
     exit;
 }
 
+//dati dei commenti
+$nPagina = $_REQUEST['nPagina'] ?? 1;
+$commenti = getCommentsFromForumId($forum_id, $nPagina, 10);
+
 //dati del file
 $files = getFilesByForumId($forum_id);
 
@@ -34,12 +38,46 @@ $files = getFilesByForumId($forum_id);
 <html lang="it">
     <head></head></head>
         <title>Forum: <?=$forum['titolo']?></title>
+        <script src="librerie/htmx.min.js"></script>
     </head>
 
     <body>
         <h1><?=$forum['titolo']?></h1>
-        <h3>creato da <?=$forum['username']?> il <?=$forum['data_pubblicazione']?></h3>
+        <h3>creato da <?=$forum['username']?> il <?=$forum['data_pubblicazione']?> alle <?=$forum['ora_pubblicazione']?></h3>
         <p><?=$forum['contenuto']?></p>
+
+        <table>
+            <tr>
+                <th>contenuto</th>
+                <th>crato da</th>
+                <th>creato il</th>
+            </tr>
+            <?php
+            foreach($commenti as $commento) {
+                ?>
+                <tr>
+                    <td><?=$commento['contenuto']?></td>
+                    <td><?=$commento['username']?></td>
+                    <td><?=$commento['data_pubblicazione']?> alle <?=$commento['ora_pubblicazione']?></td>
+                </tr>
+                <?php
+                if($commento['num_risposte'] > 0) {
+                    ?>
+                    <tr>
+                        <td colspan="3">
+                            <button
+                                hx-get="get_replies.php?commento_id=<?=$commento['commento_id']?>&nPagina=1"
+                                hx-target="#replies_<?=$commento['commento_id']?>"
+                                hx-swap="afterend">Visualizza risposte (<?=$commento['num_risposte']?>)
+                            </button>
+                            <div id="replies_<?=$commento['commento_id']?>"></div>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            }
+            ?>
+        </table>
     </body>
 </html>
 
