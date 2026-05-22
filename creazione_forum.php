b028701ccd094e905970920e893923584a73d114
Samuele <samueledb06@gmail.com>
2026-05-22 14:54:45 +0200

    Aggiunta del controllo del campo session

--- creazione_forum.php
+++ creazione_forum.php
@@ -4,11 +4,15 @@ require_once "function.php";
 
 session_start();
 
-$utente_id = $_REQUEST['utente_id'] ?? null;
+$utente_id = $_SESSION['utente_id'] ?? null;
 $titolo = $_REQUEST['titolo'] ?? null;
 $contenuto = $_REQUEST['contenuto'] ?? null;
 
-if($utente_id === null || $titolo === null || $contenuto === null) {
+if($utente_id === null) {
+    header('Location: login.php');
+    exit;
+}
+else if($titolo === null || $contenuto === null) {
     header('Location: pagina_creazione_forum.php?errore=true');
     exit;
 }
