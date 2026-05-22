f8f328cceb16c4e65ac4a7d47726c37c3524718e
samuele de bona <samueledb06@gmail.com>
2026-05-22 11:40:31 +0200

    Miglioramento della logica di aggiunta di un post

--- creazioneforum.php
+++ creazioneforum.php
@@ -9,24 +9,3 @@ if($email === null) {
     exit;
 }
 
-$utente_id = getUserIdByEmail($email);
-
-?>
-<html>
-    <head>
-        <title>Creazione forum</title>
-    </head>
-
-    <body>
-        <h1>Creazione forum</h1>
-
-        <form action="creazioneforum_action.php" method="post">
-            <label for="titolo">Titolo</label>
-            <input type="text" name="titolo" id="titolo" required>
-            <br>
-            <label for="contenuto">Contenuto</label>
-            <textarea name="contenuto" id="contenuto" required></textarea>
-            <br>
-        </form>
-    </body>
-</html>
