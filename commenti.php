71ca92edf0895c4d7f102cd3db88136a46a2c980
Samuele <samueledb06@gmail.com>
2026-05-28 22:15:03 +0200

    Miglioramento dei commenti in commenti.php

--- commenti.php
+++ commenti.php
@@ -1,17 +1,28 @@
 <?php
 require_once "function.php";
 
+// accesso negato se l'utennte non è registrato
+$email = $_SESSION['email'] ?? null;
+if ($email === null) {
+    die("Accesso negato");
+}
+
+// dati relativi a forum o commento
 $forum_id = isset($_REQUEST['forum_id']) ? (int)$_REQUEST['forum_id'] : null;
 $commento_id = isset($_REQUEST['commento_id']) ? (int)$_REQUEST['commento_id'] : null;
 $nPagina = isset($_REQUEST['nPagina']) ? (int)$_REQUEST['nPagina'] : 1;
 
-if($commento_id == null){
+if($commento_id == null and $forum_id != null) {
     $commenti = getCommentsFromForumId($forum_id, $nPagina, 11);
 }
-else{
+else if($commento_id != null) {
     $commenti = getCommentsFromCommentId($commento_id, $nPagina, 11);
 }
+else {
+    die("Parametri non validi");
+}
 
+// se non ci sono commenti, inizializziamo un array vuoto per evitare errori
 $commenti = $commenti ?? [];
 
 // controllo se è l'ultima pagina
